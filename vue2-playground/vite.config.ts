fa05762eb0c62777abcbdf79aba83fa61e774e12
ChuHoMan <74137084+ChuHoMan@users.noreply.github.com>
2024-09-09 11:50:10 +0800

    chore: update deps (#19)
    * chore: update deps
    
    * fix: fix Vue2 CompositionAPI CJS import

--- vue2-playground/vite.config.ts
+++ vue2-playground/vite.config.ts
@@ -3,8 +3,25 @@ import { defineConfig } from 'vite';
 import { createVuePlugin as vue2 } from 'vite-plugin-vue2';
 import { baseBuildConfig, getSharedPlugins } from '../vite.base.config';
 
+function fixCjsCompositionApi(): Plugin {
+  return {
+    name: 'vite-plugin-zyb-login:fix-cjs-composition-api',
+    apply: 'build',
+    enforce: 'post',
+    generateBundle(options, bundle) {
+      if (options.format === 'cjs') {
+        Object.keys(bundle).forEach((key) => {
+          if (key === 'index.cjs.js' && bundle[key].type === 'chunk') {
+            (bundle[key] as OutputChunk).code = (bundle[key] as OutputChunk).code.replaceAll('@vue/composition-api/dist/vue-composition-api.mjs', '@vue/composition-api');
+          }
+        });
+      }
+    },
+  };
+}
+
 export const viteVue2Config = defineConfig({
-  plugins: [vue2(), ...getSharedPlugins('v2')],
+  plugins: [vue2(), ...getSharedPlugins('v2'), fixCjsCompositionApi()],
   server: {
     port: 2000,
   },
@@ -13,6 +30,7 @@ export const viteVue2Config = defineConfig({
       'vue': resolve(__dirname, './node_modules/vue/dist/vue.runtime.esm.js'),
       'vue-demi': resolve(__dirname, '../node_modules/vue-demi/lib/v2/index.mjs'),
     },
+    dedupe: ['vue', '@vue/composition-api'],
   },
   ...baseBuildConfig,
 });
